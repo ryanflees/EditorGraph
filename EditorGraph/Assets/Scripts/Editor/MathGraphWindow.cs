191d91ef5902dbfd7439d370d3f1a63d86645b16
ryanflees <ryanbai2008@163.com>
2018-05-15 18:48:50 +0800

    wefwe

--- EditorGraph/Assets/Scripts/Editor/MathGraphWindow.cs
+++ EditorGraph/Assets/Scripts/Editor/MathGraphWindow.cs
@@ -9,6 +9,8 @@
     {
         private Vector2 mScrollPosition;
         private List<MathExpression> mExpressionList = new List<MathExpression>();
+        private float mCellHeight = 100f;
+        private float mCellMenuWidth = 200f;
 
         [MenuItem("AceSea/MathGraph/Open Panel")]
         private static void OpenPanel()
@@ -31,16 +33,35 @@
 
             for (int i = 0; i < mExpressionList.Count; i++)
             {
-                DrawExpression(mExpressionList[i]);
+                DrawExpression(i);
             }
 
             DrawMenu();
             EditorGUILayout.EndScrollView();
         }
 
-        private void DrawExpression(MathExpression expression)
+        private void DrawExpression(int index)
         {
+            float cellWidth = position.width * 0.97f;
+            float cellMenuWidth = 200f;
+            EditorGUILayout.BeginVertical("box", GUILayout.Width(cellWidth), GUILayout.Height(mCellHeight));
 
+            EditorGUILayout.BeginHorizontal();
+
+            EditorGUILayout.BeginVertical("box", GUILayout.Width(cellMenuWidth));
+            mExpressionList[index] = EditorGUILayout.ObjectField(mExpressionList[index], typeof(MathExpression), true) as MathExpression;
+
+            if (GUILayout.Button("Reset Value"))
+            {
+                mExpressionList[index].ResetValue();
+                Repaint();
+            }
+            if (GUILayout.Button("Remove"))
+            {
+                mExpressionList.RemoveAt(index);
+                return;
+            }
+            EditorGUILayout.EndVertical();
         }
 
         private void DrawMenu()
@@ -48,9 +69,14 @@
             EditorGUILayout.BeginVertical("box", GUILayout.Width(position.width * 0.95f));
 
             EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Cell Height", GUILayout.Width(70f));
+            mCellHeight = EditorGUILayout.FloatField(mCellHeight);
+
+            EditorGUILayout.LabelField("Cell Menu Width", GUILayout.Width(100f));
+            mCellMenuWidth = EditorGUILayout.FloatField(mCellMenuWidth);
             if (GUILayout.Button("Load Scene Expressions"))
             {
-
+                
             }
             EditorGUILayout.Space();
             if (GUILayout.Button("Load Selected Expressions"))
---
+++ EditorGraph/Assets/Scripts/Templates/LinearExpression.cs
@@ -0,0 +1,16 @@
+﻿namespace AceSea
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class LinearExpression : MathExpression
+    {
+        public float ConstValue = 1;
+
+        public override float CalculateFunction(float x)
+        {
+            return ConstValue;
+        }
+    }
+}
